45ea7a43f42ed991ce80f39b76743054de72a21f
Fabio Mantelli <fmmantelli@gmail.com>
2020-01-29 22:23:17 -0300

    defini as grids para o body2 (profile) e para o body3 (company)

--- src/components/conteudo/company/Company.js
+++ src/components/conteudo/company/Company.js
@@ -1,17 +1,57 @@
 import React, { Component } from 'react';
 
 import 'normalize.css';
-import '../../../App.css';
+import './Styles.css';
 
 
-class Conteudo extends Component {
+class Company extends Component {
     render () {
         return (
-            <div>
-                <h1>Tela 2!</h1>
+            <div className="body-company">
+                <div className="company-overview">
+                    <div className="main">
+                        Icone
+                    </div>
+
+                    <div className="icons">
+                        Icones
+                    </div>
+                </div>
+
+                <div className="options">
+                    <div className="marketing">
+                        Marketing
+                    </div>
+
+                    <div className="finance">
+                        Finance
+                    </div>
+
+                    <div className="human-resorces">
+                        Human Resorces
+                    </div>
+                </div>
+
+                <div className="itens">
+                    <div className="stress-details">
+                        Sterss-details
+                    </div>
+
+                    <div className="activity-details">
+                        Activity-details
+                    </div>
+
+                    <div className="sleep-details">
+                        Sleep-details
+                    </div>
+
+                    <div className="meditation-details">
+                        Meditation-details
+                    </div>
+                </div>
             </div>
         );
     }
 }
 
-export default Conteudo;
+export default Company;
--- src/components/conteudo/profile/Profile.js
+++ src/components/conteudo/profile/Profile.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
 
 import 'normalize.css';
-import '../../../App.css';
+import './Styles.css';
+
 
 class Conteudo extends Component {
     render () {
         return (
-            <div>
-                <h1>Tela1!</h1>
+            <div className="body-profile">
+                <div className="user">
+                    <div className="profile">
+                        <h1>Profile</h1>
+                    </div>
+                    
+                    <div className="personal">
+                        <h1>Personal</h1>
+                    </div>
+                </div>
+
+                <div className="information">
+                    <div className="log">
+                        <h1>Information</h1>
+                    </div>
+
+                    <div className="team-member">
+                        <h1>Team-Member</h1>
+                    </div>
+
+                    <div className="key-metrics">
+                        <h1>Key-Metrics</h1>
+                    </div>
+                </div>
             </div>
         );
     }
